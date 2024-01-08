30a69ef1697a02a397c4483b578f81faf4f69331
Eugene Yan <enegue84@gmail.com>
2024-01-08 22:09:46 +0800

    error-handling middleware

--- src/app.js
+++ src/app.js
@@ -5,4 +5,16 @@ const app = express();
 
 app.use('/api/places', placesRouter);
 
+app.use((error, req, res, next) => {
+  console.log(
+    `Error-handling middlware start: ${error}, headersSent: ${res.headersSent}`
+  );
+  if (res.headersSent) {
+    return next(error);
+  }
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || 'An unknown error occurred.' });
+}); // error-handling middleware function
+
 app.listen(3000);
