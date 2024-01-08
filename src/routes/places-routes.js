5555febf7ddc5155d85eecf872c5f78693b24e38
Eugene Yan <enegue84@gmail.com>
2024-01-08 21:47:00 +0800

    get by placeId, get places by userId, error handling

--- src/routes/places-routes.js
+++ src/routes/places-routes.js
@@ -1,10 +1,42 @@
 import express from 'express';
+import { TEST_PLACES } from '../data/data.js';
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   console.log('GET request in places');
   res.json({ time: new Date(), message: 'Msg received at places routes!' });
+});
+
+router.get('/:placeId', (req, res, next) => {
+  const placeId = req.params.placeId;
+  const place = TEST_PLACES.find((p) => {
+    return p.id === Number(placeId);
+  });
+
+  if (!place) {
+    return res
+      .status(404)
+      .json({ message: 'Could not find a place with the provided id.' });
+  }
+
+  res.json({ place });
+});
+
+router.get('/user/:userId', (req, res, next) => {
+  const userId = req.params.userId;
+
+  const places = TEST_PLACES.filter((p) => {
+    return p.creator === userId;
+  });
+
+  if (places.length === 0) {
+    return res.status(404).json({
+      message: 'Could not find any places created by the provided user id.',
+    });
+  }
+
+  res.json({ places });
 });
 
 export default router;
