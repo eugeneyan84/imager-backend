8f0bfd786bc367a54925cdcd5599422bd937a34d
Eugene Yan <enegue84@gmail.com>
2024-01-09 21:55:56 +0800

    middleware to handle bad routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import { default as placesRouter } from './routes/places-routes.js';
+import HttpError from './models/http-error.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/places', placesRouter);
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route', 404);
+  throw error;
+});
 
 app.use((error, req, res, next) => {
   console.log(
