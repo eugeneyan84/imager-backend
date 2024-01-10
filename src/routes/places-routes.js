a86bb43e9b6ea65bda3867b9869f98ff7781a26b
Eugene Yan <enegue84@gmail.com>
2024-01-10 17:54:21 +0800

    integrate validator on create-place route

--- src/routes/places-routes.js
+++ src/routes/places-routes.js
@@ -1,4 +1,6 @@
-import express from 'express';
+import { Router } from 'express';
+import { check } from 'express-validator';
+
 import {
   createPlace,
   deletePlace,
@@ -7,13 +9,21 @@ import {
   updatePlace,
 } from '../controllers/places-controller.js';
 
-const router = express.Router();
+const router = Router();
 
 router.get('/:placeId', getPlaceById);
 
 router.get('/user/:userId', getPlacesByUserId);
 
-router.post('/', createPlace);
+router.post(
+  '/',
+  [
+    check('title').not().isEmpty(),
+    check('description').isLength({ min: 20 }),
+    check('address').not().isEmpty(),
+  ],
+  createPlace
+);
 
 router.patch('/:placeId', updatePlace);
 
