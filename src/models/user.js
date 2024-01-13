b61491970604fe46d900b065f4628dead463575f
Eugene Yan <enegue84@gmail.com>
2024-01-13 16:30:49 +0800

    removed uniqueValidator as not working as intended

--- src/models/user.js
+++ src/models/user.js
@@ -1,14 +1,18 @@
 import { model, Schema } from 'mongoose';
-import { uniqueValidator } from '@ladjs/mongoose-unique-validator';
+//import uniqueValidator from '@ladjs/mongoose-unique-validator';
 
 const userSchema = new Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   password: { type: String, required: true, minlength: 6 },
   imageUrl: { type: String, required: true },
   places: { type: String, required: true },
 });
 
-userSchema.plugin(uniqueValidator);
+//userSchema.plugin(uniqueValidator);
 
 export default model('User', userSchema);
